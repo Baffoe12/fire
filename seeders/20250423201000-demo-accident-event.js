092dc1a53994bff1861a791bf3f9201c3e99c3e3
Your Name <you@example.com>
2025-05-10 20:16:22 +0000

    Fix: Add sample sensor and accident data seeders and update backend server

--- seeders/20250423201000-demo-accident-event.js
+++ seeders/20250423201000-demo-accident-event.js
@@ -4,7 +4,7 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert("accident_events", [
       {
-        id: "test-accident-1",
+        id: Date.now().toString(36) + Math.random().toString(36).substr(2, 5),
         alcohol: 0.05,
         vibration: 1.2,
         distance: 15.5,
@@ -18,6 +18,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete("accident_events", { id: "test-accident-1" });
+    await queryInterface.bulkDelete("accident_events", null, {});
   }
 };
